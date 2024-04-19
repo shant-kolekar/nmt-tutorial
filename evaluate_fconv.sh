7d7f3b3600edb0640f3d7cc6845bf9b20cc1f8ca
Shant <97169131+shant-kolekar@users.noreply.github.com>
2024-04-19 14:35:48 -0400

    Update evaluate_fconv.sh with proper sacrebleu cmd

--- evaluate_fconv.sh
+++ evaluate_fconv.sh
@@ -16,19 +16,20 @@ CPKT=checkpoint/$MODEL_${SRC}_${TRG}
 RESULT=result/$MODEL_${SRC}_${TRG}
 mkdir -p $RESULT
 
-CUDA_VISIBLE_DEVICES=$GPU fairseq-interactive $DATA_BIN \
-    --path $CPKT/checkpoint_best.pt \
+CUDA_VISIBLE_DEVICES=$GPU fairseq-interactive data-bin/fconv_en_as/ \
+    --path checkpoint/fconv_en_as/checkpoint_best.pt \
     --buffer-size 2000 --batch-size 128 \
     --beam 5 --remove-bpe \
     --input $TEXT/test.${SRC} | tee $RESULT/wmt23.test.${SRC}-${TRG}.${TRG}.sys
 
 #(8) Post-processing	
-# Extract translations from output file 
+echo "Extract translations from output file"
 grep ^H $RESULT/wmt23.test.${SRC}-${TRG}.${TRG}.sys | cut -f3- > $RESULT/hypo.tok.sys
 
-# Remove tokenization and truecasing
+echo " Remove tokenization and truecasing"
 cat $RESULT/hypo.tok.sys | $SCRIPTS/recaser/detruecase.perl | $SCRIPTS/tokenizer/detokenizer.perl -l ${TRG} > $RESULT/hypo.sys
 
 #(9) Automatic Evaluation
-# Score the test set with sacrebleu
-sacrebleu --tok=13a $RESULT/hypo.sys < en-as-test-$TRG.txt > $RESULT/sacrebleu.sys
+echo "Score the test set with sacrebleu"
+sacrebleu --tok=13a $RESULT/hypo.sys en-as-test-$TRG.txt > $RESULT/sacrebleu.sys
+
